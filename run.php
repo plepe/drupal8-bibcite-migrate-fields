2064393c024e2b2c9d4e658fe69eb16332e35abc
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-10-09 11:22:19 +0200

    Iterate over all nodes of type 'biblio'

--- run.php
+++ run.php
@@ -15,12 +15,21 @@ require_once DRUPAL_DIR . '/core/includes/schema.inc';
 
 $fields = array('field_ieg_ike_project', 'field_projects', 'field_videolink', 'field_suppl');
 
-  $src = \Drupal::entityTypeManager()->getStorage('node')->load(824);
-  $dest = \Drupal::entityTypeManager()->getStorage('bibcite_reference')->load(466);
+$nids = \Drupal::entityQuery('node')->condition('type', 'biblio')->execute();
+$bibid = 0;
+foreach ($nids as $nid) {
+  $bibid++;
+
+  print "Copy {$nid} to {$bibid}\n";
+  $src = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+  $dest = \Drupal::entityTypeManager()->getStorage('bibcite_reference')->load($bibid);
 
   foreach ($fields as $field) {
+    print "{$field}: load ... ";
     $value = $src->get($field)->getValue();
+    print "save\n";
     $dest->get($field)->setValue($value);
   }
 
   $dest->save();
+}
