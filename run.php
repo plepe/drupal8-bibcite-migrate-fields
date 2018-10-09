3c07a5601ac385e1af7bebdd32d75653c65012b3
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-10-09 10:04:49 +0200

    Iterate over fields

--- run.php
+++ run.php
@@ -13,22 +13,14 @@ $kernel->boot();
 require_once DRUPAL_DIR . '/core/includes/database.inc';
 require_once DRUPAL_DIR . '/core/includes/schema.inc';
 
-  $src = \Drupal::entityTypeManager()->getStorage('node')->load(836);
-  $dest = \Drupal::entityTypeManager()->getStorage('bibcite_reference')->load(473);
+$fields = array('field_ieg_ike_project', 'field_projects', 'field_videolink', 'field_suppl');
 
-  print_r($src);
-
-  $fields = array('field_teaserimage', 'field_ieg_ike_project', 'field_projects', 'field_videolink', 'field_suppl');
+  $src = \Drupal::entityTypeManager()->getStorage('node')->load(824);
+  $dest = \Drupal::entityTypeManager()->getStorage('bibcite_reference')->load(466);
 
   foreach ($fields as $field) {
     $value = $src->get($field)->getValue();
-    print_r($value);
-
-
-  //  print_r($dest->get($field)->getValue());
     $dest->get($field)->setValue($value);
-  //  print_r($dest->get($field)->getValue());
-    //print_r($entity);
   }
 
   $dest->save();
